1ec5d8f69bd415af932c2de58e818b60f1707a9a
Aniket <aniketlawande@gmail.com>
2024-08-30 08:36:34 +0530

    Created http server by inheriting tcp server

--- sHTTP.js
+++ sHTTP.js
@@ -1,37 +1,16 @@
-const net = require('net');
-const { Buffer } = require('node:buffer');
+import { TcpServer } from "./TcpServer";
 
+class sHTTP extends TcpServer {
 
-const server = net.createServer((conn) => {
-    let data = '';
-    const resp = `HTTP/1.1 200 OK
-                Date: Mon, 27 Jul 2009 12:28:53 GMT
-                Server: sHTTP v0.5
-                Last-Modified: Thu, 29 Aug 2024 19:15:56 GMT
-                Content-Length: 88
-                Content-Type: text/html
+    listen(port) {
+        this.server.listen(port, () => {
+            console.log("sHTTP listening on port:", port);
+        })
+    }
+    reqHandler(data, conn) {
+        console.log("sHTTP is handling this now");
+        super.reqHandler(data, conn);
+    }
+}
 
-
-                Hello world! 
-                sHTTP greets you!
-                `;
-
-    conn.on('data', (stream) => {
-        data += stream;
-        console.log("Data received");
-        conn.write(Buffer.from(resp));
-        conn.end();
-    });
-    conn.on('end', ()=> {
-        conn.end();
-        console.log('Client has disconnected');    
-    });
-})
-
-server.on("error", (err) => {
-    console.log(err);
-})
-
-server.listen(8080, () => {
-    console.log("sHTTP is up and running!");
-})
+const server = new sHTTP(8080);
